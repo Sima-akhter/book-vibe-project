c5646c14fbb3247075ab12ae4a0cb4258f2721ab
Sima-akhter <saimaislambdk@gmail.com>
2026-04-09 23:05:17 +0600

    added book details page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,6 +3,7 @@ import MainLayout from "../Layout/MainLayout";
 import HomePages from "../pages/homePages/HomePages";
 import Books from "../pages/books/Books";
 import ErrorPages from "../pages/errorPages/ErrorPages";
+import BookDetails from "../pages/bookDetails/BookDetails";
 
 export const router = createBrowserRouter([
   {
@@ -16,7 +17,11 @@ export const router = createBrowserRouter([
       {
         path: "/books",
         element: <Books></Books>
-      }
+      },
+      {
+        path:"/bookDetails/:id",
+        element: <BookDetails></BookDetails>,
+      },
     ],
     errorElement: <ErrorPages></ErrorPages>
   },
